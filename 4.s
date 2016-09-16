3152e2eb989c538bec0a398599f8c50f142c8104
Patrick <pxcland@gmail.com>
2016-09-16 19:51:00 +0900

    Add files via upload

---
+++ 4.s
@@ -0,0 +1,64 @@
+# Project Euler Problem 4
+# Largest palindrome product
+# Written by Patrick Cland
+
+
+# t0 is constant 999
+# t1 is number 1
+# t2 is number 2
+# a1 is address of variable
+	.text
+main:
+	li $t0, 999
+	li $t1, 99		#starts at 3 digit numbers
+	la $a1, num
+	
+loop1:
+	addiu $t1, $t1, 1		#increment loop for number 1
+	bge   $t1, $t0, done 	#999 is the bound of the loop
+	li    $t2, 99			#reset 2nd loop counter
+loop2:
+	addiu $t2, $t2, 1		#increment loop for number 2
+	bge   $t2, $t0, loop1
+	mul   $v0, $t1, $t2
+	jal   isPalindrome		#v1 is return value
+	beqz  $v1, loop2		#if not a palindrome, get the next number
+	lw    $v1, ($a1)		#make sure we only overwrite the number if it is greater than the current one
+	blt   $v0, $v1, loop2
+	sw    $v0, ($a1)		#if a palindrome, save it and overwrite the last
+	j     loop2
+	
+done:
+	lw $a0, ($a1)			#print
+	li $v0, 1
+	syscall
+	li $v0, 10
+	syscall
+	
+	
+	#takes argument in $v0
+	#returns 0 or 1 in $v1
+isPalindrome:
+	move $t9, $v0		#save a copy, as we will need it later
+	li   $v1, 0			#stores the reversed numer for now
+	li   $t8, 10
+isPalindromeLoop:
+	rem   $t7, $v0, $t8	#modulus 10 to get the least significant digit
+	mul   $v1, $v1, $t8	#multiply by 10 to move the digits up a place 
+	addu  $v1, $v1, $t7	#and add the least significant digit
+	div   $v0, $v0, $t8	#divide by 10 to get the next digit above
+	blez  $v0, isPalindromeDone
+	j isPalindromeLoop
+isPalindromeDone:
+	move $v0, $t9		#callee restores v0
+	beq  $v1, $t9, yes	#if the reversed = original, is a palindrome
+	li   $v1, 0
+	jr   $ra
+yes:
+	li   $v1, 1
+	jr   $ra
+	
+	.data
+num:
+.space 4
+	
---
+++ 5.s
@@ -0,0 +1,34 @@
+# Project Euler Problem 5
+# Smallest multiple
+# Written by Patrick Cland
+
+# t0 stores the dividend
+# t1 the divisor counter, 1-20
+# t2 stores 20, the boundary of the loop
+
+	.text
+main:
+	li $t0, 0
+	li $t2, 10
+	
+increment:
+	addiu $t0, $t0, 10	#increment the dividend by 20, anything less is redundant
+	li    $t1, 1		#reset the divisor
+	
+testDivisible:
+	rem  $t9, $t0, $t1
+	beqz $t9, evenDiv	#if this was an even division, try the next
+	j    increment
+	
+evenDiv:
+	addiu $t1, $t1, 1		#increment the divisor
+	bgt   $t1, $t2, done	#if the divisor is greater than 20, test if the number is prime
+	j     testDivisible		#and test it
+	
+done:
+	move $a0, $t0	#print and exit
+	li   $v0, 1
+	syscall
+	li   $v0, 10
+	syscall
+	
